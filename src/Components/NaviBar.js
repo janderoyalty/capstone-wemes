87a9d32eb6ca2b69da45d863dea67e320e19f08d
Jande Royalty <janderoyalty@gmail.com>
2022-08-16 09:22:30 -0500

    adds logo to NaviBar

--- src/Components/NaviBar.js
+++ src/Components/NaviBar.js
@@ -7,18 +7,27 @@ import Accounts from "./Pages/Accounts";
 import Home from "./Pages/Home";
 import Transactions from "./Pages/Transactions";
 import Items from "./Pages/Items";
+// import "../wemes_logo.png"
 
 const NaviBar = ({ accounts, transactions }) => {
   const wemes_url = "https://wemes-be.herokuapp.com/";
   // const wemes_url = "http://127.0.0.1:8000/";
-
+  // const Img = <img src={/wemes_logo.png}/>;
+// 
   return (
     <Router>
       <div>
         <Navbar bg="warning">
           <Container justify="right">
-            <Navbar.Brand as={Link} to={"/"}>
-              Wemes
+            <Navbar.Brand  as={Link} to={"/"}>
+            <img
+              src={require('../wemes_logo.png')}
+              // width="30"
+              height="40"
+              className="d-inline-block align-top"
+              alt="Wemes logo"
+            />
+            {/* Wemes */}
             </Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link as={Link} to={"/accounts"}>
